d61e4be2c80e585134ff979ff72666373e7999c0
Robin Bürkli <robuba.jr@gmx.ch>
2021-12-15 15:32:43 +0100

    Remove rollup plugin terser
    - Let consumers do the minifying, so they have access to jsdoc.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/lib.ts',
@@ -10,5 +9,5 @@ export default {
       format: 'es',
     },
   ],
-  plugins: [typescript(), terser()],
+  plugins: [typescript()],
 };
